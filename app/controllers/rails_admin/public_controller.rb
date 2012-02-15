7b1370e4407a4f4c7b81ad6cd4a0b6e172c96de8
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-02-15 10:36:34 +0100

    index público

--- app/controllers/rails_admin/public_controller.rb
+++ app/controllers/rails_admin/public_controller.rb
@@ -3,10 +3,13 @@ module RailsAdmin
     skip_before_filter :_authenticate!
     skip_before_filter :_authorize!
     before_filter :get_model
-    before_filter :get_object
+    before_filter :get_object, :only => :show
 
     def show
       render :action => 'rails_admin/main/show'
     end
-  end
+
+    def index
+      @objects = @abstract_model.model.order('updated_at DESC').limit(10)
+    end    
 end
